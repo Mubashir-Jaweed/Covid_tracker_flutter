d47f919a6d512a35993abd8dc24118ae27d119c4
MubashirJ <mubashirmjjawed@gmail.com>
2024-05-01 19:31:43 +0500

    completed

---
+++ lib/screens/home.dart
@@ -0,0 +1,175 @@
+import 'dart:convert';
+
+import 'package:d_chart/commons/config_render.dart';
+import 'package:d_chart/commons/data_model.dart';
+import 'package:d_chart/ordinal/pie.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool isLoading = true;
+  String Country = 'Pakistan';
+  Map<String, dynamic> res = {};
+
+  @override
+  void initState() {
+    getData(Country);
+    super.initState();
+  }
+
+  void getData(country) async {
+    isLoading = true;
+    final String url = 'https://covid-193.p.rapidapi.com/history';
+    final Map<String, String> headers = {
+      "X-RapidAPI-Key": "f45d8848eamshad6366977ca5fe3p1c8858jsnb2a47996e921",
+      "X-RapidAPI-Host": "covid-193.p.rapidapi.com"
+    };
+
+    final Map<String, String> params = {"country": country};
+
+    Uri uri = Uri.parse(url);
+    uri = uri.replace(queryParameters: params);
+
+    try {
+      final response = await http.get(uri, headers: headers);
+      res = await json.decode(response.body);
+
+      if (res.isNotEmpty) {
+        setState(() {
+          isLoading = false;
+        });
+      }
+    } catch (e) {
+      print(e);
+      setState(() {
+        isLoading = true;
+      });
+    }
+  }
+
+  List<OrdinalData> ordinalDataList = [
+    OrdinalData(domain: 'Population', measure: 0, color: Colors.grey[300]),
+    OrdinalData(domain: 'Cases', measure: 0, color: Colors.lightBlue),
+    OrdinalData(domain: 'Deaths', measure: 0, color: Colors.red),
+  ];
+
+  List<Map> analytics = [
+    {"name": "Confirmed", "value": 0, "color": Colors.grey[500]},
+    {"name": "Recovered", "value": 0, "color": Colors.lightBlue},
+    {"name": "Deaths", "value": 0, "color": Colors.red},
+    {"name": "0 Cases", "value": 0, "color": Colors.black},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      drawer: Drawer(),
+      body: isLoading
+          ? Container(
+              child: Center(
+                child: Column(
+                  children: [CircularProgressIndicator()],
+                ),
+              ),
+            )
+          : Container(
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Pakistan Case's",
+                    style: TextStyle(
+                      color: Colors.grey[900],
+                      fontSize: 35,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Text(
+                    "2024-05-01",
+                    style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.grey),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: AspectRatio(
+                      aspectRatio: 10 / 10,
+                      child: DChartPieO(
+                        data: ordinalDataList,
+                        configRenderPie: const ConfigRenderPie(arcWidth: 50),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                      child: GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        mainAxisSpacing: 20,
+                        crossAxisSpacing: 15,
+                        childAspectRatio: 2),
+                    itemCount: analytics.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                              width: 1,
+                              color: const Color.fromARGB(255, 218, 218, 218),
+                            ),
+                            borderRadius: BorderRadius.circular(5)),
+                        padding: EdgeInsets.all(10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.analytics,
+                                  size: 18,
+                                  color: analytics[index]['color'],
+                                ),
+                                SizedBox(
+                                  width: 5,
+                                ),
+                                Text(
+                                  analytics[index]['name'],
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500,
+                                      color: analytics[index]['color']),
+                                ),
+                              ],
+                            ),
+                            Text(
+                              analytics[index]['value'].toString(),
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.black),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ))
+                ],
+              ),
+            ),
+    );
+  }
+}
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,72 @@
+import 'dart:async';
+
+import 'package:covidtracker/screens/home.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Timer.periodic(Duration(seconds: 2), (timer) {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => Home()),
+      );
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/splash.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Container(
+            height: 200,
+            width: 200,
+            color: Color.fromARGB(93, 255, 255, 255),
+            padding: EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Be aware, Stay healthy",
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.black,
+                  ),
+                ),
+                Text(
+                  "Welcome to COVID-78.6 information portal.",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
